462e9e4bb8343f44031db06ad7dfb4d866db52ca
unknown <pismecho@gmail.com>
2021-05-07 15:07:19 +0500

    Сделал тестовое

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	import Block from "./Block.svelte";
+	let blocks = [
+		{ id: 1, color: 'red', opacity: 'opacity'},
+		{ id: 2, color: 'blue', opacity: 'opacity'},
+		{ id: 3, color: 'green', opacity: 'opacity'},
+	]
+	function setOpacity (e){
+		console.log(blocks[e.target.id-1])
+		if(blocks.some(el => el.opacity === "opacity")){
+			if(blocks[e.target.id-1].opacity == "opacity"){
+				blocks[e.target.id-1].opacity = "notOpacity"
+			} else {
+				blocks[e.target.id-1].opacity = "opacity"
+			}
+		} else{
+			blocks = blocks.map(el=>{
+				el.opacity="opacity"
+				return el
+			})
+			console.log(blocks)
+		}
+
+	}
+</script>
+
+<div on:click={setOpacity} class="container">
+	{#each blocks as block}
+	<Block  color={block.color} id={block.id} opacity={block.opacity}/>
+	{/each}
+</div>
+
+
+
+<style>
+	.container {
+		width: 100px;
+		height: 300px;
+	}
+</style>
---
+++ src/Block.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let color = 'green';
+    export let opacity;
+    export let id;
+</script>
+
+<div class="block {color} {opacity}" id={id}>
+
+</div>
+
+<style>
+    .block {
+        width: 100px;
+        height: 100px;
+        border: 1px solid black;
+    }
+    .green{
+    background-color: green;
+    }
+    .red{
+        background-color: red;
+    }
+    .blue{
+        background-color: blue;
+    }
+    .opacity{
+        opacity: 1;
+    }
+    .notOpacity{
+        opacity: 0;
+    }
+</style>
